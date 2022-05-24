bfd30559417be6b1c24369c8d834033d13e6dc39
AndySu1021 <ivy841021@gmail.com>
2022-05-25 03:22:46 +0800

    fix: add graphql exporter

---
+++ exporter/graphql.go
@@ -0,0 +1,162 @@
+package exporter
+
+import (
+	"context"
+	"time"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	existStatusFailure = "failure"
+	exitStatusSuccess  = "success"
+)
+
+var (
+	requestStartedCounter    prometheus.Counter
+	requestCompletedCounter  prometheus.Counter
+	resolverStartedCounter   *prometheus.CounterVec
+	resolverCompletedCounter *prometheus.CounterVec
+	timeToResolveField       *prometheus.HistogramVec
+	timeToHandleRequest      *prometheus.HistogramVec
+)
+
+type (
+	GraphQLTracer struct{}
+)
+
+var _ interface {
+	graphql.HandlerExtension
+	graphql.OperationInterceptor
+	graphql.ResponseInterceptor
+	graphql.FieldInterceptor
+} = GraphQLTracer{}
+
+func GraphQLRegister() {
+	GraphQLRegisterOn(prometheus.DefaultRegisterer)
+}
+
+func GraphQLRegisterOn(registerer prometheus.Registerer) {
+	requestStartedCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "graphql_request_started_total",
+			Help: "Total number of requests started on the graphql server.",
+		},
+	)
+
+	requestCompletedCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "graphql_request_completed_total",
+			Help: "Total number of requests completed on the graphql server.",
+		},
+	)
+
+	resolverStartedCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "graphql_resolver_started_total",
+			Help: "Total number of resolver started on the graphql server.",
+		},
+		[]string{"object", "field"},
+	)
+
+	resolverCompletedCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "graphql_resolver_completed_total",
+			Help: "Total number of resolver completed on the graphql server.",
+		},
+		[]string{"object", "field"},
+	)
+
+	timeToResolveField = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "graphql_resolver_duration_ms",
+		Help:    "The time taken to resolve a field by graphql server.",
+		Buckets: prometheus.ExponentialBuckets(1, 2, 11),
+	}, []string{"exitStatus", "object", "field"})
+
+	timeToHandleRequest = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "graphql_request_duration_ms",
+		Help:    "The time taken to handle a request by graphql server.",
+		Buckets: prometheus.ExponentialBuckets(1, 2, 11),
+	}, []string{"exitStatus"})
+
+	registerer.MustRegister(
+		requestStartedCounter,
+		requestCompletedCounter,
+		resolverStartedCounter,
+		resolverCompletedCounter,
+		timeToResolveField,
+		timeToHandleRequest,
+	)
+}
+
+func GraphQLUnRegister() {
+	GraphQLUnRegisterFrom(prometheus.DefaultRegisterer)
+}
+
+func GraphQLUnRegisterFrom(registerer prometheus.Registerer) {
+	registerer.Unregister(requestStartedCounter)
+	registerer.Unregister(requestCompletedCounter)
+	registerer.Unregister(resolverStartedCounter)
+	registerer.Unregister(resolverCompletedCounter)
+	registerer.Unregister(timeToResolveField)
+	registerer.Unregister(timeToHandleRequest)
+}
+
+func (a GraphQLTracer) ExtensionName() string {
+	return "Prometheus"
+}
+
+func (a GraphQLTracer) Validate(schema graphql.ExecutableSchema) error {
+	return nil
+}
+
+func (a GraphQLTracer) InterceptOperation(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
+	requestStartedCounter.Inc()
+	return next(ctx)
+}
+
+func (a GraphQLTracer) InterceptResponse(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+	errList := graphql.GetErrors(ctx)
+
+	var exitStatus string
+	if len(errList) > 0 {
+		exitStatus = existStatusFailure
+	} else {
+		exitStatus = exitStatusSuccess
+	}
+
+	oc := graphql.GetOperationContext(ctx)
+	observerStart := oc.Stats.OperationStart
+
+	timeToHandleRequest.With(prometheus.Labels{"exitStatus": exitStatus}).
+		Observe(float64(time.Since(observerStart).Nanoseconds() / int64(time.Millisecond)))
+
+	requestCompletedCounter.Inc()
+
+	return next(ctx)
+}
+
+func (a GraphQLTracer) InterceptField(ctx context.Context, next graphql.Resolver) (interface{}, error) {
+	fc := graphql.GetFieldContext(ctx)
+
+	resolverStartedCounter.WithLabelValues(fc.Object, fc.Field.Name).Inc()
+
+	observerStart := time.Now()
+
+	res, err := next(ctx)
+
+	var exitStatus string
+	if err != nil {
+		exitStatus = existStatusFailure
+	} else {
+		exitStatus = exitStatusSuccess
+	}
+
+	timeToResolveField.WithLabelValues(exitStatus, fc.Object, fc.Field.Name).
+		Observe(float64(time.Since(observerStart).Nanoseconds() / int64(time.Millisecond)))
+
+	resolverCompletedCounter.WithLabelValues(fc.Object, fc.Field.Name).Inc()
+
+	return res, err
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/AndySu1021/go-util
 go 1.18
 
 require (
+	github.com/99designs/gqlgen v0.17.7
 	github.com/bsm/redislock v0.7.2
 	github.com/cenkalti/backoff/v4 v4.1.2
 	github.com/gin-contrib/cors v1.3.1
@@ -11,6 +12,7 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang/mock v1.6.0
 	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.11.1
 	github.com/rs/xid v1.4.0
 	github.com/rs/zerolog v1.26.1
 	go.etcd.io/etcd/client/v3 v3.5.4
@@ -20,6 +22,7 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
@@ -35,10 +38,15 @@ require (
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
+	github.com/vektah/gqlparser/v2 v2.4.3 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
@@ -50,12 +58,12 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
-	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	google.golang.org/grpc v1.38.0 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
